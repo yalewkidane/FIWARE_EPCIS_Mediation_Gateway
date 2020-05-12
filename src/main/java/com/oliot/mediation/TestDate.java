98403eecb2f3ffd26e09520c10902281128fe915
root <root@localhost.localdomain>
2020-05-12 08:38:26 -0400

    jar file updated

--- src/main/java/com/oliot/mediation/TestDate.java
+++ src/main/java/com/oliot/mediation/TestDate.java
@@ -1,14 +1,14 @@
-package com.oliot.mediation;
-
-import java.util.Calendar;
-
-public class TestDate {
-	public static void main(String[] args) {
-		Calendar cal = Calendar.getInstance();
-		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
-		int hourOfDay = cal.get(Calendar.HOUR_OF_DAY);
-		Double hourOfDayDouble =Double.valueOf(hourOfDay);
-		System.out.println(hourOfDay);
-		System.out.println(hourOfDayDouble);
-	}
-}
+//package com.oliot.mediation;
+//
+//import java.util.Calendar;
+//
+//public class TestDate {
+//	public static void main(String[] args) {
+//		Calendar cal = Calendar.getInstance();
+//		int dayOfMonth = cal.get(Calendar.DAY_OF_MONTH);
+//		int hourOfDay = cal.get(Calendar.HOUR_OF_DAY);
+//		Double hourOfDayDouble =Double.valueOf(hourOfDay);
+//		System.out.println(hourOfDay);
+//		System.out.println(hourOfDayDouble);
+//	}
+//}
